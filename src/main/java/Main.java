cbe8446c2391fdc4b24b1fc50bd990a8d63ea46d
Nguyet <nguyet@Zipcoders-MacBook-Pro-103.local>
2023-03-01 16:13:32 -0500

    ask user for name and print out in console

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        System.out.println("Please input your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        if(userInput.equalsIgnoreCase("alice") || userInput.equalsIgnoreCase("bob")){
+            System.out.format("Hello %s",userInput);
+        }
 
     }
 }
